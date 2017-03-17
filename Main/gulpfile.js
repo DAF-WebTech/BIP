2750faf30407c042ff282f8925abee172d31b9e4
David Porter <david.porter@daf.qld.gov.au>
2017-03-17 13:20:37 +1000

    included modernizr in gulp and its output

--- Main/gulpfile.js
+++ Main/gulpfile.js
@@ -2,12 +2,17 @@ var gulp = require("gulp");
 var concat = require('gulp-concat');
 var minify = require("gulp-minify");
 
-gulp.task("js", function(){
+gulp.task("js-scripts", function(){
 	return gulp.src(["source/js/*.js", "!source/js/*.min.js"])
 		.pipe(minify({noSource: true}))
 		.pipe(concat("scripts.min.js"))
 		.pipe(gulp.dest("./dist"));
 });
 
+gulp.task("js-modernizr", function(){
+	return gulp.src("source/js/modernizr.js")
+		.pipe(minify({noSource: true}))
+		.pipe(gulp.dest("./dist"));
+});
 
-gulp.task("default", [ "js" ]);
+gulp.task("default", [ "js-scripts", "js-modernizr" ]);
