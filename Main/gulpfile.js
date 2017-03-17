d8f82f79c5957759d105a23c831a5d69e5c1baf2
David Porter <david.porter@daf.qld.gov.au>
2017-03-17 14:09:21 +1000

    change to script task

--- Main/gulpfile.js
+++ Main/gulpfile.js
@@ -3,7 +3,7 @@ var concat = require('gulp-concat');
 var minify = require("gulp-minify");
 
 gulp.task("js-scripts", function(){
-	return gulp.src(["source/js/globals.js", "source/js/*.js", "!source/js/modernizr.js"])
+	return gulp.src(["source/js/global.js", "source/js/*.js", "!source/js/modernizr.js"])
 		.pipe(minify({noSource: true}))
 		.pipe(concat("scripts.min.js"))
 		.pipe(gulp.dest("./dist"));
